f3fe0172134695f9d1444f9ea8aa6af257a8be7a
Bjornson <arthurbearnyson@mail.ru>
2020-10-06 16:45:20 +0600

    Updates

---
+++ precache-manifest.7d29125d5ec011e01ddba0bb10a61814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097087834d443cca955c3bd59317582a",
+    "url": "/abstracts/index.html"
+  },
+  {
+    "revision": "8fcee49c36427d597644",
+    "url": "/abstracts/static/css/main.e2332600.chunk.css"
+  },
+  {
+    "revision": "3e04c353973088876690",
+    "url": "/abstracts/static/js/2.392ab1ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/abstracts/static/js/2.392ab1ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fcee49c36427d597644",
+    "url": "/abstracts/static/js/main.273cc9d9.chunk.js"
+  },
+  {
+    "revision": "ddfdc15f9b24c759a54f",
+    "url": "/abstracts/static/js/runtime-main.081f47d9.js"
+  }
+]);
